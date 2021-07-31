a13cf2bd6febb21a5dcd96910112143afd98ba1b
Ablavatskiy <Ablavatskiy@adani.by>
2021-07-31 19:19:41 +0300

    [+] Added TimePicker

--- Sources/DestinyNet/VIewModels/EventEditorViewModel.cs
+++ Sources/DestinyNet/VIewModels/EventEditorViewModel.cs
@@ -4,10 +4,23 @@ using System.Windows.Input;
 
 namespace DestinyNet
 {
+    public class RepeatRule
+    {
+        public RepeatRule(RepeatTypes repeatType, string name)
+        {
+            RepeatType = repeatType;
+            Name = name;
+        }
+        public RepeatTypes RepeatType { get; set; }
+        public string Name { get; set; }
+    }
+
     public class EventEditorViewModel : DialogBaseViewMode
     {
         private readonly Data _data;
+        private RepeatRule _selectedRepeatRules;
         private Dictionary<RepeatTypes, IViewModel> _viewModelsDictionary;
+
         public EventEditorViewModel(ICommand closeWindowCommand, Data data) : base(closeWindowCommand)
         {
             _data = data;
@@ -17,26 +30,61 @@ namespace DestinyNet
             _viewModelsDictionary.Add(RepeatTypes.Week, new WeekEditorViewModel());
             _viewModelsDictionary.Add(RepeatTypes.Mounth, new MounthEditorViewModel());
             _viewModelsDictionary.Add(RepeatTypes.Year, new YearEditorViewModel());
-            RepeatType = RepeatTypes.Days;
+
+            RepeatRules = new ObservableCollection<RepeatRule>();
+            RepeatRules.Add(new RepeatRule(RepeatTypes.None, ""));
+            RepeatRules.Add(new RepeatRule(RepeatTypes.Days, "Повторять по дням"));
+            RepeatRules.Add(new RepeatRule(RepeatTypes.Week, "Повторять по неделям"));
+            RepeatRules.Add(new RepeatRule(RepeatTypes.Mounth, "Повторять по месяцам"));
+            RepeatRules.Add(new RepeatRule(RepeatTypes.Year, "Повторять по годам"));
+            SelectedRepeatRules = RepeatRules[0];
         }
         public ObservableCollection<Calendar> Calendars { get => _data.Calendars; }
         public Calendar SelectedCalendar { get; set; }
-        public IViewModel SelectedRepeatViewModel { get => _viewModelsDictionary[RepeatType]; }
-        public RepeatTypes RepeatType { get; set; }
+
+        public IViewModel SelectedRepeatViewModel { get => _viewModelsDictionary[SelectedRepeatRules.RepeatType]; }
+
+        public RepeatRule SelectedRepeatRules
+        {
+            get => _selectedRepeatRules;
+            set
+            {
+                SetField(ref _selectedRepeatRules, value);
+                OnPropertyChanged(nameof(SelectedRepeatViewModel));
+            }
+        }
+        public ObservableCollection<RepeatRule> RepeatRules { get; set; }
 
     }
 
     public class DaysEditorViewModel : BaseViewModel
     {
+        public int Step { get; set; }
+        public bool IsDayStep { get; set; }
+        public bool IsWorkDayStep { get; set; }
+        public int RepeatCount { get; set; }
     }
     public class WeekEditorViewModel : BaseViewModel
     {
+        public int Step { get; set; }
+        public bool IsMonday { get; set; }
+        public bool IsTuesday { get; set; }
+        public bool IsWednesday { get; set; }
+        public bool IsThursday { get; set; }
+        public bool IsFriday { get; set; }
+        public bool IsSaturday { get; set; }
+        public bool IsSunday { get; set; }
+        public int RepeatCount { get; set; }
     }
     public class MounthEditorViewModel : BaseViewModel
     {
+        public int Step { get; set; }
+        public int RepeatCount { get; set; }
     }
     public class YearEditorViewModel : BaseViewModel
     {
+        public int Step { get; set; }
+        public int RepeatCount { get; set; }
     }
 
 }
---
+++ Sources/UI.MonthControl/TimePicker.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace UIMonthControl
+{
+    [TemplatePart(Name = TimePicker.TP_GRID, Type = typeof(FrameworkElement))]
+    [TemplatePart(Name = TimePicker.TP_TITLE_PICKER, Type = typeof(FrameworkElement))]
+    [TemplatePart(Name = TimePicker.TP_TITLE_PICKER_POPUP, Type = typeof(FrameworkElement))]
+    [TemplatePart(Name = TimePicker.TP_TITLE_PICKER_BUTTON, Type = typeof(FrameworkElement))]
+    public class TimePicker : Control
+    {
+        private Grid _grid;
+        private TextBox _TimePicker;
+        private Popup _TimePickerPopup;
+        private Button _TimePickerButton;
+        private const string TP_GRID = "xGrid";
+        private const string TP_TITLE_PICKER = "xTimePicker";
+        private const string TP_TITLE_PICKER_POPUP = "xTimePickerPopup"; 
+        private const string TP_TITLE_PICKER_BUTTON = "xTimePickerButton";
+        static TimePicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TimePicker), new FrameworkPropertyMetadata(typeof(TimePicker)));
+        }
+        ~ TimePicker()
+        {
+            if (_TimePickerButton != null)
+                _TimePickerButton.Click -= DoClick;
+            if (_TimePicker != null)
+                _TimePicker.KeyDown -= DoKeyDown;
+            if (_TimePickerPopup != null)
+                _TimePickerPopup.LostFocus -= DoLostFocus;
+        }
+
+        #region Date
+        public static readonly DependencyProperty DateProperty =
+            DependencyProperty.Register("Date", typeof(DateTime), typeof(TimePicker), new PropertyMetadata(DatePropertyChanged));
+
+        public static void DatePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((TimePicker)d).Date = (DateTime)e.NewValue;
+        }
+        public DateTime Date
+        {
+            get { return (DateTime)GetValue(DateProperty); }
+            set
+            {
+                SetValue(DateProperty, value);
+            }
+        }
+        #endregion
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _grid = (Grid)GetTemplateChild(TP_GRID);
+            _TimePicker = (TextBox)GetTemplateChild(TP_TITLE_PICKER);
+            _TimePickerPopup = (Popup)GetTemplateChild(TP_TITLE_PICKER_POPUP);
+            _TimePickerButton = (Button)GetTemplateChild(TP_TITLE_PICKER_BUTTON);
+            _TimePickerButton.Click += DoClick;
+            _TimePicker.KeyDown += DoKeyDown;
+            _TimePickerPopup.LostFocus += DoLostFocus;
+        }
+        private void DoClick(object sender, RoutedEventArgs e)
+        {
+            _TimePickerPopup.IsOpen = !_TimePickerPopup.IsOpen;
+            if (_TimePickerPopup.IsOpen)
+                FocusManager.SetFocusedElement(_TimePickerPopup, _TimePickerPopup);
+        }
+
+        private void DoLostFocus(object sender, RoutedEventArgs e) 
+        {
+            _TimePickerPopup.IsOpen = false;
+        }
+        public void DoKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
